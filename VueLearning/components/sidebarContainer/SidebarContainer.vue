2fbbfbcb1a01cf12153463a0bc7257cb1aa38160
Balázs Tolvaj <tolvibalazs@gmail.com>
2022-02-23 10:44:23 +0100

    Implement AdminLte Boostrap template
    #15

---
+++ VueLearning/components/sidebarContainer/SidebarContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <Logo/>
+    <div class="sidebar">
+
+    </div>
+  </aside>
+</template>
+
+<script>
+import Logo from "@/components/sidebarContainer/Logo";
+export default {
+  name: "SidebarContainer",
+  components: {Logo}
+}
+</script>
+
+<style scoped>
+
+</style>
