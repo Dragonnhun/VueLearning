50d2d065d86d2484690c6d023d83fe639dad0eb1
Balázs Tolvaj <tolvibalazs@gmail.com>
2022-05-07 20:49:24 +0200

    Redux added

---
+++ nextlearning/src/features/dashboard/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+      <div>
+          <footer className="main-footer">
+              <strong></strong>
+              <div className="float-right d-none d-sm-inline-block">
+                  <b>Version:</b>
+              </div>
+          </footer>
+      </div>
+    );
+};
+
+export default Footer
---
+++ nextlearning/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Footer from "../features/dashboard/Footer";
+
+
+const IndexPage: NextPage = () => {
+    return (
+        <div className="sidebar-mini layout-fixed sidebar-height dark-mode sidebar-collapse">
+            <div className="wrapper">
+                <div className="content-wrapper">
+                    <span>Content here</span>
+                </div>
+                <Footer/>
+            </div>
+        </div>
+)
+}
+
+export default IndexPage
