47404816057cee39dae7150693ea26f9b9cf8a3d
Ádám Jakab <adam.jakab@lombiq.com>
2022-05-07 22:10:41 +0200

    Adding MongoDB cluster connection

--- nextlearning/src/pages/index.tsx
+++ nextlearning/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Footer from "../features/dashboard/Footer";
+import Footer from "../features/dashboard/Footer"
+import clientPromise from '../../lib/mongodb';
 
 
 const IndexPage: NextPage = () => {
@@ -17,3 +17,26 @@ const IndexPage: NextPage = () => {
 }
 
 export default IndexPage
+
+export async function getServerSideProps(context) {
+    try {
+        await clientPromise
+        // `await clientPromise` will use the default database passed in the MONGODB_URI
+        // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+        //
+        // `const client = await clientPromise`
+        // `const db = client.db("myDatabase")`
+        //
+        // Then you can execute queries against your database like so:
+        // db.find({}) or any of the MongoDB Node Driver commands
+
+        return {
+            props: { isConnected: true },
+        }
+    } catch (e) {
+        console.error(e)
+        return {
+            props: { isConnected: false },
+        }
+    }
+}
