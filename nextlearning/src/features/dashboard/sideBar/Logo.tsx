49d82e82fe94ef707304fe5e84446efed19ce52e
Balázs Tolvaj <tolvibalazs@gmail.com>
2022-05-08 22:24:57 +0200

    user data slice

--- nextlearning/src/features/dashboard/sideBar/Logo.tsx
+++ nextlearning/src/features/dashboard/sideBar/Logo.tsx
@@ -1,15 +1,19 @@
 import {Fragment} from "react";
 import Link from 'next/link';
-import Image from "next/image";
+
+import {selectBannerText} from "../../interface/interfaceSlice";
+import {useAppSelector} from "../../../app/hooks";
 
 const Logo = () => {
+    const bannerText = useAppSelector(selectBannerText)
+
     return (
         <Fragment>
             <Link href="/" >
                 <a className="brand-link">
                     <img src="/img/AdminLTELogo.png" alt="AdminLTE Logo"
                          className="brand-image img-circle elevation-3" style={{opacity: .8}}/>
-                    <span className="brand-text font-weight-light">Simple Chat</span>
+                    <span className="brand-text font-weight-light">{bannerText}</span>
                 </a>
             </Link>
         </Fragment>
